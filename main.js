7d609cf3582ab0457370a6f99a7530840ca808bf
k3jo <joanne.kwh@gmail.com>
2017-07-19 13:46:33 -0400

    mostly styles

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var titleList = ["Honorable", "Ms.", "Mr.", "Professor", "Lady", "Duke", "Duchess"];
 var adjList = ["Messy", "Nuclear", "Sunny", "Chatty", "Childish", "Smokey", "Lucky", "Suave"];
-var nounList = ["Kiwi", "Mango", "Skittles", "Harley", "Phoenix", "Eagle", "Sammy", "Peaches", "Tweety", "Charlie", "Bebe", "Gizmo", "Noodles", "Odin", "Pixie", "Picles", "Zeus", "Goldie", "Robin"];
+var nounList = ["Kiwi", "Mango", "Skittles", "Harley", "Phoenix", "Eagle", "Sammy", "Peaches", "Tweety", "Charlie", "Bebe", "Gizmo", "Noodles", "Odin", "Pixie", "Pickles", "Zeus", "Goldie", "Robin", "Peanut"];
 var wildcardList = ["Smith", "Tailfeathers", "the First", "Pikachu", "Chirp", "Twinkles", "Bon Bon", "Chip", "Littlefoot", "Big Bird"];
 
 
@@ -23,8 +23,8 @@ $(function(){
 
 	$('button').on('click', function(e){
 		e.preventDefault();
-			$('p').empty();
-		$('p').html(fullName());	
+			$('p.gen-name').empty();
+		$('p.gen-name').html(fullName());	
 	});
 
 
